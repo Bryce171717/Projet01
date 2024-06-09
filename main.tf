a7090c5491ef04b7abb841cde4e94374157d1ca9
Bryce171717 <brice.jamain17@gmail.com>
2024-06-09 08:57:28 +0200

    Main v2

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 provider "aws" {
-  region = "eu-west-3"  # Région Europe (Paris)
+  region = "eu-west-3"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "app" {
-  ami           = "ami-052984d1804039ba8"  # A adapter en fonction des besoins
+  ami           = "ami-052984d1804039ba8"
   instance_type = "t2.micro"
   tags = {
     Name = "chatbot-app"
   }
+
   provisioner "remote-exec" {
     inline = [
       "sudo apt update -y",
