7b999d02ce7e7ddd03a047a4ef69977134f394cd
Bryce171717 <brice.jamain17@gmail.com>
2024-06-21 14:10:58 +0200

    Branche terraform v2

--- emr.tf
+++ emr.tf
@@ -1,7 +1,7 @@
 resource "aws_emr_cluster" "spark_cluster" {
   name          = "SparkCluster01"
   release_label = "emr-7.1.0"
-  applications  = ["Hadoop", "Hive", "JupyterEnterpriseGateway", "Livy", "Spark"]
+  applications  = ["JupyterEnterpriseGateway", "Hive", "Hadoop", "Livy", "Spark"]
   log_uri       = var.emr_log_uri
 
   ec2_attributes {
@@ -9,25 +9,43 @@ resource "aws_emr_cluster" "spark_cluster" {
     subnet_id                  = var.subnet_id
     emr_managed_master_security_group = var.emr_security_group_master
     emr_managed_slave_security_group  = var.emr_security_group_slave
+    key_name                   = var.emr_key_name
   }
 
+  configurations_json = <<EOF
+  [
+    {
+      "Classification": "spark-defaults",
+      "Properties": {
+        "spark.jars.packages": "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",
+        "spark.mongodb.input.uri": "mongodb://${var.mongodb_username}:${var.mongodb_password}@mongodb:27017/database01",
+        "spark.mongodb.output.uri": "mongodb://${var.mongodb_username}:${var.mongodb_password}@mongodb:27017/database01"
+      }
+    }
+  ]
+  EOF
+
   master_instance_group {
-    instance_type = var.instance_type
+    instance_type = "m5.xlarge"
     instance_count = 1
     name = "Master"
+    ebs_config {
+      ebs_block_device_config {
+        volume_specification {
+          volume_type = "gp2"
+          size        = 32
+        }
+        volumes_per_instance = 2
+      }
+    }
   }
 
   core_instance_group {
-    instance_type = var.instance_type
+    instance_type = "m5.xlarge"
     instance_count = 2
     name = "Core"
-  }
-
-  scale_down_behavior = "TERMINATE_AT_TASK_COMPLETION"
-
-  auto_termination_policy {
-    idle_timeout = 3600
-  }
-
-  service_role = var.emr_service_role
-}
+    ebs_config {
+      ebs_block_device_config {
+        volume_specification {
+          volume_type = "gp2"
+  
--- main.tf
+++ main.tf
@@ -1,3 +1,121 @@
+# Fournisseur AWS
 provider "aws" {
-  region = var.aws_region
+  region = "eu-west-3"
+}
+
+# Variables pour les informations sensibles (à remplacer par vos valeurs)
+variable "mongodb_username" {
+  type        = string
+  description = "Nom d'utilisateur administrateur pour MongoDB"
+  sensitive   = true
+  default     = "admin01"
+}
+
+variable "mongodb_password" {
+  type        = string
+  description = "Mot de passe administrateur pour MongoDB"
+  sensitive   = true
+  default     = "1234"
+}
+
+# Cluster EMR (Apache Spark)
+resource "aws_emr_cluster" "Spark_Cluster" {
+  name          = "SparkCluster01"
+  release_label = "emr-7.1.0"
+  applications  = ["Hadoop", "Hive", "JupyterEnterpriseGateway", "Livy", "Spark"]
+  service_role = "arn:aws:iam::031131961798:role/service-role/AmazonEMR-ServiceRole-20240505T140225"
+  log_uri       = "s3://aws-logs-031131961798-eu-west-3/elasticmapreduce"
+
+  ec2_attributes {
+    instance_profile              = "arn:aws:iam::031131961798:instance-profile/EmrEc2S3Full"
+    subnet_id                     = "subnet-00508a1e4ac0faf28"
+    emr_managed_master_security_group = "sg-01ceb80944ffa01d6"
+    emr_managed_slave_security_group  = "sg-0852ccf141b26f385"
+    key_name                       = "SSH_Mongodb"
+  }
+
+  master_instance_group {
+    instance_type  = "m5.xlarge"
+    instance_count = 1
+    name           = "Primaire"
+  }
+
+  core_instance_group {
+    instance_type  = "m5.xlarge"
+    instance_count = 2
+    name           = "Unité principale et unité de tâches"
+  }
+
+  # Configuration du connecteur MongoDB (avec IP privée)
+  configurations_json = jsonencode([
+    {
+      Classification = "spark-defaults",
+      Properties     = {
+        "spark.jars.packages"   = "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",
+        "spark.mongodb.input.uri"  = "mongodb://${var.mongodb_username}:${var.mongodb_password}@${aws_instance.mongodb.private_ip}:27017/database01",
+        "spark.mongodb.output.uri" = "mongodb://${var.mongodb_username}:${var.mongodb_password}@${aws_instance.mongodb.private_ip}:27017/database01"
+      }
+    }
+  ])
+
+  scale_down_behavior = "TERMINATE_AT_TASK_COMPLETION"
+  auto_termination_policy {
+    idle_timeout = 3600
+  }
+}
+
+# Instance EC2 MongoDB
+resource "aws_instance" "mongodb" {
+  ami           = "ami-087da76081e7685da"
+  instance_type = "m5.large"
+  key_name      = "SSH_Mongodb"
+
+  # Stockage EBS pour MongoDB
+  root_block_device {
+    volume_size = 50
+  }
+
+  vpc_security_group_ids = ["sg-01ceb80944ffa01d6", "sg-0852ccf141b26f385"]
+
+  # Script d'installation et de configuration de MongoDB
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y mongodb-org
+    # Configuration de MongoDB (authentification, etc.)
+    sudo systemctl start mongod
+    sudo systemctl enable mongod
+  EOF
+
+  tags = {
+    Name = "MongoDB Server"
+  }
+}
+
+# Bucket S3 pour stocker les données
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "S3_datas" 
+
+  # Configuration du cycle de vie des objets, chiffrement, etc.
+}
+
+# Sorties
+output "cluster_id" {
+  value = aws_emr_cluster.Spark_Cluster.id
+}
+
+output "master_public_dns" {
+  value = aws_emr_cluster.Spark_Cluster.master_public_dns
+}
+
+output "mongodb_instance_status" {
+  value = aws_instance.mongodb.instance_state
+}
+
+output "mongodb_private_ip" {
+  value = aws_instance.mongodb.private_ip
+}
+
+output "data_bucket_arn" {
+  value = aws_s3_bucket.data_bucket.arn
 }
